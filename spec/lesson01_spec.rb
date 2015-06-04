809007e5401886d0af67ee11a4de9c6171ba32cc
Julio Betta <juliobetta@gmail.com>
2015-06-04 13:39:56 -0300

    finished task 1 and 2

---
+++ spec/lesson01_spec.rb
@@ -0,0 +1,32 @@
+require 'rspec'
+Dir["./lesson01/*.rb"].each {|file| require file }
+
+describe 'Lesson 1' do
+  context 'FROG JUMP' do
+    it 'returns the right result' do
+      expect(FrogJump.solution(10, 85, 30)).to eq 3
+    end
+  end
+
+  context 'MISSING ELEMENT' do
+    context 'with more than 1 elements' do
+      it 'returns the right result' do
+        expect(MissingElement.solution([2,3,1,5])).to eq 4
+      end
+    end
+
+    context 'with 1 element' do
+      it 'returns the n+1 element' do
+        expect(MissingElement.solution([1])).to eq 2
+      end
+    end
+
+    context 'with non-array or empty array' do
+      it 'returns 1' do
+        [[], 1].each do |el|
+          expect(MissingElement.solution(el)).to eq 1
+        end
+      end
+    end
+  end
+end
